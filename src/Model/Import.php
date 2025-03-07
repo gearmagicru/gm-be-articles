17084791a160f078915a10faca511446a685697c
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-07 23:26:41 +0300

    Обновлён Gm\Backend\Articles\Model

--- src/Model/Import.php
+++ src/Model/Import.php
@@ -30,77 +30,192 @@ class Import extends \Gm\Import\Import
     {
         return [
             // идентификатор материала
-            'type_id' => ['field' => 'type_id', 'type' => 'int'],
+            'type_id' => [
+                'field' => 'type_id', 
+                'type'  => 'int'
+            ],
             // идентификатор категории материала
-            'category_id' => ['field' => 'category_id', 'type' => 'int'],
+            'category_id' => [
+                'field' => 'category_id', 
+                'type'  => 'int'
+            ],
             // идентификатор языка
-            'language_id' => ['field' => 'language_id', 'type' => 'int'],
+            'language_id' => [
+                'field' => 'language_id', 
+                'type'  => 'int'
+            ],
             // порядковый номер
-            'index' => ['field' => 'index', 'type' => 'int'],
+            'index' => [
+                'field' => 'index', 
+                'type'  => 'int'
+            ],
             // количество посещений
-            'hits' => ['field' => 'hits', 'type'  => 'int'],
+            'hits' => [
+                'field' => 'hits', 
+                'type'  => 'int'
+            ],
             // фиксация материала в списке
-            'fixed' => ['field' => 'fixed', 'type'  => 'int'],
+            'fixed' => [
+                'field' => 'fixed', 
+                'type'  => 'int'
+            ],
             // файл шаблона материала
-            'template' => 'template',
+            'template' => [
+                'field'  => 'template',
+                'length' => 255,
+                'trim'   => true
+            ],
             // файл шаблона страницы
-            'page_template' => 'page_template',
+            'page_template' => [
+                'field'  => 'page_template',
+                'length' => 255,
+                'trim'   => true
+            ],
             // пути к медиа ресурсам
-            'media_paths' =>'media_paths',
+            'media_paths' => [
+                'field' => 'media_paths',
+                'trim'  => true
+            ],
             // указан в тексте шорткод
-            'has_shortcode' => ['field' => 'has_shortcode', 'type' => 'int'],
+            'has_shortcode' => [
+                'field' => 'has_shortcode', 
+                'type'  => 'int'
+            ],
             // опубликовать материал на сайте
-            'publish'=> ['field' => 'publish', 'type'  => 'int'], 
+            'publish' => [
+                'field' => 'publish', 
+                'type'  => 'int'
+            ], 
             // опубликовать материал на главной странице
-            'publish_on_main' => ['field' => 'publish_on_main', 'type'  => 'int'],
+            'publish_on_main' => [
+                'field' => 'publish_on_main', 
+                'type'  => 'int'
+            ],
             // опубликовать материал в разделах сайта
-            'publish_in_categories' => ['field' => 'publish_in_categories', 'type' => 'int'], 
+            'publish_in_categories' => [
+                'field' => 'publish_in_categories', 
+                'type' => 'int'
+            ], 
             // дата и время публикации материала
-            'publish_date' => 'publish_date',
+            'publish_date' => [
+                'field' => 'publish_date',
+                'trim'  => true
+            ],
             // заголовок материала
-            'header' => 'header',
+            'header' => [
+                'field'  => 'header',
+                'length' => 255,
+                'trim'   => true
+            ],
             // изображение материала
-            'image' => 'image',
+            'image' => [
+                'field'  => 'image',
+                'length' => 255,
+                'trim'   => true
+            ],
             // текст материала
-            'text' => 'text',
+            'text' => [
+                'field' => 'text',
+                'trim'  => true
+            ],
             // анонс материала 
-            'announce' => 'announce',
+            'announce' => [
+                'field' => 'announce',
+                'trim'  => true
+            ],
             // ярлык (слаг)
-            'slug' => 'slug',
+            'slug' => [
+                'field'  => 'slug',
+                'length' => 255,
+                'trim'   => true
+            ],
             // вид ярлыка (слага)
             'slug_type' => 'slug_type',
             // материал участвует в поиске
-            'in_search' => ['field' => 'in_search', 'type' => 'int'],  
+            'in_search' => [
+                'field' => 'in_search', 
+                'type'  => 'int'
+            ],  
             // материал выводится на карте сайта
-            'in_sitemap' => ['field' => 'in_sitemap', 'type' => 'int'],
+            'in_sitemap' => [
+                'field' => 'in_sitemap', 
+                'type'  => 'int'
+            ],
             // карта сайта (Sitemap) включает материал
-            'sitemap_enabled' => ['field' => 'sitemap_enabled', 'type' => 'int'],
+            'sitemap_enabled' => [
+                'field' => 'sitemap_enabled', 
+                'type'  => 'int'
+            ],
             // карта сайта (Sitemap), приоритетность (от 0.0 до 1.0)
-            'sitemap_priority' => ['field' => 'sitemap_priority', 'type' => 'float'],
+            'sitemap_priority' => [
+                'field' => 'sitemap_priority', 
+                'type'  => 'float'
+            ],
             // карта сайта (Sitemap), частота изменения
-            'sitemap_frequency' => 'sitemap_frequency', 
+            'sitemap_frequency' => [
+                'field'  => 'sitemap_frequency', 
+                'length' => 10,
+                'trim'   => true
+            ],
             // материал выводится в фид (лента новостей, анонсов статей в формате RSS, RDF, Atom):
-            'feed_enabled'=> ['field' => 'feed_enabled', 'type' => 'int'],
+            'feed_enabled'=> [
+                'field' => 'feed_enabled', 
+                'type'  => 'int'
+            ],
             // метатег материала title
-            'title' =>'title',
+            'title' => [
+                'field' => 'title',
+                'length' => 255,
+                'trim'   => true
+            ],
             // метатег материала keywords
-            'keywords' => 'meta_keywords',
+            'keywords' => [
+                'field' => 'meta_keywords',
+                'trim'  => true
+            ],
             // метатег материала description
-            'description' => 'meta_description',
+            'description' => [
+                'field' => 'meta_description',
+                'trim'  => true
+            ],
             // метатег robots
-            'robots' => 'meta_robots',
+            'robots' => [
+                'field'  => 'meta_robots',
+                'length' => 50,
+                'trim'   => true
+            ],
             // метатег googlebot
-            'googlebot' => 'meta_googlebot',
+            'googlebot' => [
+                'field'  => 'meta_googlebot',
+                'length' => 50,
+                'trim'   => true
+            ],
             // метатег yandex
-            'yandex' => 'meta_yandex',
+            'yandex' => [
+                'field'  => 'meta_yandex',
+                'length' => 50,
+                'trim'   => true
+            ],
             // все остальные метатеги (JSON формат)
-            'meta_tags' => 'meta_tags',
+            'meta_tags' => [
+                'field' => 'meta_tags',
+                'trim'  => true
+            ],
             // кэшировать
-            'caching' => ['field' => 'caching', 'type' => 'int'],
+            'caching' => [
+                'field' => 'caching', 
+                'type'  => 'int'
+            ],
             // поля (JSON формат)
-            'fields' => 'fields',
+            'fields' => [
+                'field' => 'fields',
+                'trim'  => true
+            ],
             // метки через разделитель ","
-            'tags' => 'tags'
+            'tags' => [
+                'field' => 'tags',
+                'trim'  => true
+            ]
         ];
     }
 
@@ -109,17 +224,9 @@ class Import extends \Gm\Import\Import
      */
     protected function afterImportAttributes(array $columns): array
     {
-        // текст материала
-        if (!empty($columns['text'])) {
-            $columns['text'] = trim($columns['text']);
-        }
         // текст материала без форматирования
         if (!empty($columns['text'])) {
             $columns['text_plain'] = strip_tags($columns['text']);
-        }
-        // анонс материала
-        if (!empty($columns['announce'])) {
-            $columns['announce'] = trim($columns['announce']);
         }
         // анонс материала без форматирования
         if (!empty($columns['announce'])) {
